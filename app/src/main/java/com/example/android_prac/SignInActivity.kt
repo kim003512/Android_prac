cac3dcb6941d194838398d42e090a91dde9be96d
min_jjiny <kim003512@naver.com>
2020-03-05 20:14:54 +0900

    submit signin, signup

--- app/src/main/java/com/example/android_prac/SignInActivity.kt
+++ app/src/main/java/com/example/android_prac/SignInActivity.kt
@@ -1,2 +1,18 @@
 package com.example.android_prac
 
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SignInActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signin)
+
+        initialUI()
+    }
+
+    private fun initialUI(){
+
+    }
+}
--- app/src/main/java/com/example/android_prac/SignUpActivity.kt
+++ app/src/main/java/com/example/android_prac/SignUpActivity.kt
@@ -1,2 +1,13 @@
 package com.example.android_prac
 
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState : Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+
+}
