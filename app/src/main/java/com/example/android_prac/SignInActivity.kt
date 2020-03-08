8a77da9351a145a5cac8aef7d0e1539bf892b89f
min_jjiny <kim003512@naver.com>
2020-03-08 15:24:07 +0900

    submit signin, signup

--- app/src/main/java/com/example/android_prac/SignInActivity.kt
+++ app/src/main/java/com/example/android_prac/SignInActivity.kt
@@ -1,7 +1,9 @@
 package com.example.android_prac
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_signin.*
 
 class SignInActivity : AppCompatActivity() {
 
@@ -14,5 +16,10 @@ class SignInActivity : AppCompatActivity() {
 
     private fun initialUI(){
         //https://reqres.in/ 서버 연결
+        btn_signup.setOnClickListener {
+            var signup_intent = Intent(this, SignUpActivity::class.java)
+            startActivity(signup_intent)
+
+        }
     }
 }
