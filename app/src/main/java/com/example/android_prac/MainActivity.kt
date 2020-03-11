64342c4ab445a1b2de619d903f533866022afe61
min_jjiny <kim003512@naver.com>
2020-03-11 22:39:14 +0900

    submit signin, signup

--- app/src/main/java/com/example/android_prac/MainActivity.kt
+++ app/src/main/java/com/example/android_prac/MainActivity.kt
@@ -10,9 +10,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        btn1.setOnClickListener {
-            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
-        }
     }
 }
--- app/src/main/java/com/example/android_prac/SignInActivity.kt
+++ app/src/main/java/com/example/android_prac/SignInActivity.kt
@@ -2,6 +2,7 @@ package com.example.android_prac
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_signin.*
 
@@ -15,11 +16,22 @@ class SignInActivity : AppCompatActivity() {
     }
 
     private fun initialUI(){
+        var edt_id = et_id.text
+        var edt_pw = et_pw.text
+
+        var main_intent = Intent(this, MainActivity::class.java)
         //https://reqres.in/ 서버 연결
         btn_signup.setOnClickListener {
             var signup_intent = Intent(this, SignUpActivity::class.java)
             startActivity(signup_intent)
 
+        }
+
+        btn_signin.setOnClickListener {
+            if(edt_id.isEmpty() && edt_pw.isEmpty())
+                Toast.makeText(this, "빈칸을 모두 입력해주세요!", Toast.LENGTH_SHORT).show()
+            else
+                startActivity(main_intent)
         }
     }
 }
