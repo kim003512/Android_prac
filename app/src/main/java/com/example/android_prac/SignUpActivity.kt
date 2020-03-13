6cacd3de490268889f2de676fd55dbd5d6c69dc6
min_jjiny <kim003512@naver.com>
2020-03-14 01:57:19 +0900

    submit signin, signup

--- app/src/main/java/com/example/android_prac/SignUpActivity.kt
+++ app/src/main/java/com/example/android_prac/SignUpActivity.kt
@@ -2,6 +2,10 @@ package com.example.android_prac
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_signin.*
+import kotlinx.android.synthetic.main.activity_signin.et_id
+import kotlinx.android.synthetic.main.activity_signin.et_pw
+import kotlinx.android.synthetic.main.activity_signup.*
 
 class SignUpActivity : AppCompatActivity() {
 
@@ -11,12 +15,18 @@ class SignUpActivity : AppCompatActivity() {
 
         //유효성 검사
         //val edit_email =
+        var edt_id = et_id.text
+        var edt_pw = et_pw.text
+        var edt_name = et_name.text
+        var edt_phone = et_phone.text
 
-        //이메일
+        //아이디, 비밀번호, 이름, 번호
 
         //비밀번호
 
-        //닉네임?
+        //이름
+
+        //ㅂ먼호
     }
 
 }
