3096ed485ba1c78a894be27cc3b5584de39ae26a
min_jjiny <kim003512@naver.com>
2020-03-25 23:37:37 +0900

    prac fcm

---
+++ app/src/main/java/com/example/MyFirebaseMessagingService.kt
@@ -0,0 +1,16 @@
+package com.example
+
+import android.annotation.SuppressLint
+
+//class MyFirebaseMessagingService : FirebaseMessagingService(){
+//    override fun onMessageReceived(remoteMessage: RemoteMessage?){
+//        @SuppressLint("LongLogTAG")
+//        if(remoteMessage?.notification != null){
+//            sendNotification(remoteMessage.notification?.title, remoteMessage.notification!!.body!!)
+//        }
+//    }
+//
+//    private fun sendNotification(title: String?, body:String){
+//
+//    }
+//}
