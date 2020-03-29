5fe928dbb0b9b6c1b91e21a7542a1b776d535b67
min_jjiny <kim003512@naver.com>
2020-03-29 23:32:15 +0900

    prac fcm

--- app/src/main/java/com/example/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/MyFirebaseMessagingService.kt
@@ -3,6 +3,7 @@ package com.example
 import android.annotation.SuppressLint
 import android.app.NotificationManager
 import android.app.PendingIntent
+import android.app.PendingIntent.getActivity
 import android.content.Intent
 import android.media.RingtoneManager
 import androidx.core.app.NotificationCompat
@@ -23,11 +24,10 @@ class MyFirebaseMessagingService : FirebaseMessagingService(){
 
     private fun sendNotification(title: String?, body:String){
         //어떤 모양으로 알림을 할지 설정한 다음 실제 폰 상단에 표시하도록 한다
-//        //pendingIntent를 이용 알림을 클릭하면 열 앱의 액티비티를 설정해 준다
+        // pendingIntent를 이용 알림을 클릭하면 열 앱의 액티비티를 설정해 준다
 //        val intent = Intent(this, MainActivity::class.java)
 //        var main_intent = Intent(this, MainActivity::class.java)
 //
-//        val intent = Intent(this, MainActivity::class.java)
 //        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
 //        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
 //        val defaultSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
