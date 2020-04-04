b70709a2829614dfef076525968c3a16b50892c0
min_jjiny <kim003512@naver.com>
2020-04-04 17:20:01 +0900

    꾸믈 회원가입창 만드는즁(수정 필요-간격)

--- app/src/main/java/com/example/android_prac/SignInActivity_ggumul.kt
+++ app/src/main/java/com/example/android_prac/SignInActivity_ggumul.kt
@@ -1,12 +1,19 @@
 package com.example.android_prac
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_signin_ggumul.*
 
 class SignInActivity_ggumul : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signin_ggumul)
+
+        btn_signin_finish.setOnClickListener {
+            val intent_recommand = Intent(this, RecommandActivity::class.java)
+            startActivity(intent_recommand)
+        }
     }
 }
