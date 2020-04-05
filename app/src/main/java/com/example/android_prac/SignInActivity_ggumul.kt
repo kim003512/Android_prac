a5bcb17183c1ddbaca494cbadce448ea134fc5eb
min_jjiny <kim003512@naver.com>
2020-04-05 23:19:47 +0900

    꾸믈 회원가입창 만드는즁(수정 필요-간격)

--- app/src/main/java/com/example/android_prac/SignInActivity_ggumul.kt
+++ app/src/main/java/com/example/android_prac/SignInActivity_ggumul.kt
@@ -3,6 +3,7 @@ package com.example.android_prac
 import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_signin_ggumul.*
 
@@ -11,7 +12,15 @@ class SignInActivity_ggumul : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signin_ggumul)
 
+        var name = edt_signin_name.text.toString()
+        var id = edt_signin_id.text.toString()
+        var pw = edt_signin_pw.text.toString()
+        var birth = edt_signin_birth.text.toString()
+
         btn_signin_finish.setOnClickListener {
+            if(name.isEmpty() || id.isEmpty() || pw.isEmpty() || birth.isEmpty()){
+                Toast.makeText(this, "빈칸을 입력하세요!", Toast.LENGTH_SHORT).show()
+            }
             val intent_recommand = Intent(this, RecommandActivity::class.java)
             startActivity(intent_recommand)
         }
